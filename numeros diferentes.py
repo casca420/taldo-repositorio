c30d6f8077056f9ecd508a92d8d5ba3806d0c6ed
casca420 <beandradeluz1010@gmail.com>
2025-07-17 10:39:27 -0300

    Add files via upload

---
+++ numeros diferentes.py
@@ -0,0 +1,14 @@
+print("=== Comparar dois números ==")
+# Ler doi números da tela
+a = int(input("Digite um número"))
+b = int(input("Digite outro número"))
+#Comprar os dois números
+if(a == b):
+    print("Números iguais!")
+else:
+    print("Números diferentes!")
+#Mostrar o número maior
+if(a > b):
+    print("O maior é a = ",a)
+else:
+    print("O menor é b = ",b)
